102457155e40b8227b779715219036cc91c973d0
darshandc123 <darshandc202@gmail.com>
2023-08-17 12:40:11 +0530

    change1

---
+++ 2.py
@@ -0,0 +1,6 @@
+import numpy as np
+a=np.array([[6,7,4],[2,5,6]])
+b=np.array([[2,5,6],[6,7,4]])
+print("adding the matrix =",a+b)
+print("sustracting the matrix =",a-b)
+print("multiplying the matrix =",a*b)
